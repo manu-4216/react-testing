5c130898791abc74517970dbaaeaa53b27d45229
Manuel Micu <manu4216@gmail.com>
2018-06-05 01:51:33 +0200

    Add form inside App

---
+++ note2self/src/components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            text: ''
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Note to Self</h2>
+                <Form inline>
+                    <FormControl
+                        onChange={event =>
+                            this.setState({ text: event.target.value })
+                        }
+                    />{' '}
+                    <Button
+                        onClick={() => {
+                            console.log(this.state.text);
+                        }}
+                    >
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default App;

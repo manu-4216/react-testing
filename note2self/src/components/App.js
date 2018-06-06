a8379ddcd81bd7827aeac14db01f96225f0f9997
Manuel Micu <manu4216@gmail.com>
2018-06-06 02:55:25 +0200

    Add notes

--- note2self/src/components/App.js
+++ note2self/src/components/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
+import Note from './Note';
 
 class App extends Component {
     constructor() {
         super();
 
         this.state = {
-            text: ''
+            text: '',
+            notes: []
         };
+
+        this.submit = this.submit.bind(this);
+    }
+
+    submit() {
+        const { notes, text } = this.state;
+
+        notes.push({ text });
+        this.setState({ notes, text: '' });
     }
 
     render() {
@@ -19,14 +30,12 @@ class App extends Component {
                         onChange={event =>
                             this.setState({ text: event.target.value })
                         }
+                        value={this.state.text}
                     />{' '}
-                    <Button
-                        onClick={() => {
-                            console.log(this.state.text);
-                        }}
-                    >
-                        Submit
-                    </Button>
+                    <Button onClick={this.submit}>Submit</Button>
+                    {this.state.notes.map(note => (
+                        <Note key={note.text} note={note} />
+                    ))}
                 </Form>
             </div>
         );

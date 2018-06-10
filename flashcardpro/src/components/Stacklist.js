885758bd3c5c02305058260d38f05221d6280dd3
Manuel Micu <manu4216@gmail.com>
2018-06-10 20:54:58 +0200

    Add a list of mock stacks

---
+++ flashcardpro/src/components/Stacklist.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import stacks from '../data/stacks.json';
+
+class Stacklist extends Component {
+    render() {
+        return (
+            <div>
+                {stacks.map(stack => <h4 key={stack.id}>{stack.title}</h4>)}
+            </div>
+        );
+    }
+}
+
+export default Stacklist;

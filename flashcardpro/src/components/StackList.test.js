ac34974e5849e76c4a37a3b86a3949fcfa7db7da
Manuel Micu <manu4216@gmail.com>
2018-06-15 00:34:48 +0200

    Add another test into StackList

--- flashcardpro/src/components/StackList.test.js
+++ flashcardpro/src/components/StackList.test.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { StackList } from './StackList';
 import { stacks } from '../data/fixtures';
 
-const props = {
-    stacks
-};
-
 describe('StackList', () => {
-    const stackList = shallow(<StackList {...props} />);
+    describe('when it mounts', () => {
+        describe('and it has already stacks', () => {
+            const props = {
+                stacks
+            };
+            let stackList = shallow(<StackList {...props} />);
 
-    it('renders the correct number of links', () => {
-        console.log(stackList.debug());
-        expect(stackList.find('Link').length).toBe(stacks.length);
+            it('renders the existent stacks', () => {
+                expect(stackList.find('Link').length).toBe(stacks.length);
+            });
+        });
+
+        describe('and it has no stacks', () => {
+            const props = {
+                stacks: [],
+                loadStacks: jest.fn()
+            };
+            let stackList = mount(<StackList {...props} />);
+
+            it('calls `loadStacks`', () => {
+                expect(props.loadStacks).toHaveBeenCalled();
+            });
+        });
     });
 });

204272cc9ce799c5487a6e72026740e3a60dcffd
Manuel Micu <manu4216@gmail.com>
2018-06-16 20:14:15 +0200

    Refactor using stateless functional components

--- flashcardpro/src/components/Stack.js
+++ flashcardpro/src/components/Stack.js
@@ -3,22 +3,16 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Card from './Card';
 
-export class Stack extends Component {
-    render() {
-        const { title, cards } = this.props.stack;
-
-        return (
-            <div>
-                <Link to="/" className="link-home">
-                    <h4>Home</h4>
-                </Link>
-                <h3>{title}</h3>
-                <hr />
-                {cards.map(card => <Card key={card.id} card={card} />)}
-            </div>
-        );
-    }
-}
+export const Stack = ({ stack: { title, cards } }) => (
+    <div>
+        <Link to="/" className="link-home">
+            <h4>Home</h4>
+        </Link>
+        <h3>{title}</h3>
+        <hr />
+        {cards.map(card => <Card key={card.id} card={card} />)}
+    </div>
+);
 
 const mapStateToProps = state => ({
     stack: state.stack

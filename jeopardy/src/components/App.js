bab2206690a00baaa73e49d3868e2c45b6e70fae
Manuel Micu <manu4216@gmail.com>
2018-06-23 00:57:38 +0200

    Add first action, reducer

---
+++ jeopardy/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    componentDidMount() {
+        fetch('http://jservice.io/api/categories?count=20')
+            .then(response => response.json())
+            .then(json => console.log(json));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Jeopardy!</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
--- jeopardy/src/index.js
+++ jeopardy/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);

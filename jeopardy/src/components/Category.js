071833db8bd86d1104fd82461af64225ad315652
Manuel Micu <manu4216@gmail.com>
2018-06-24 19:53:10 +0200

    Add Category tests

--- jeopardy/src/components/Category.js
+++ jeopardy/src/components/Category.js
@@ -19,13 +19,23 @@ export class Category extends Component {
     render() {
         return (
             <div>
-                <Link className="link-home" to="/">
-                    <h2>Home</h2>
-                </Link>
                 <h2>{this.props.category.title}</h2>
                 {this.state.clues.map(clue => {
                     return <Clue key={clue.id} clue={clue} />;
                 })}
+            </div>
+        );
+    }
+}
+
+export class LinkedCategory extends Component {
+    render() {
+        return (
+            <div>
+                <Link className="link-home" to="/">
+                    <h4>Home</h4>
+                </Link>
+                <Category category={this.props.category} />
             </div>
         );
     }
@@ -38,4 +48,4 @@ const mapStateToProps = state => ({
 export default connect(
     mapStateToProps,
     null
-)(Category);
+)(LinkedCategory);

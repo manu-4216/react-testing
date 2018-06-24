2cd696210c8cb838f77797238cad5ac6c05abc10
Manuel Micu <manu4216@gmail.com>
2018-06-24 18:37:53 +0200

    Add test for Clue

--- jeopardy/src/data/fixtures.js
+++ jeopardy/src/data/fixtures.js
@@ -2,3 +2,10 @@ export const categories = [
     { id: 0, title: 'category one' },
     { id: 1, title: 'category two' }
 ];
+
+export const clue = {
+    id: 0,
+    question: 'q one',
+    answer: 'a one',
+    value: 200
+};
